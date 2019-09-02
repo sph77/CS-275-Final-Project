92cd8f2ff439bed2627b758b071ebd575ed340fa
jaslyon10 <38379960+jaslyon10@users.noreply.github.com>
2019-09-02 09:58:32 -0400

    updated socket

--- socket/chat.js
+++ socket/chat.js
@@ -4,18 +4,40 @@ function chat(){
 	var text = document.getElementById("input");
 	var chatbox = document.getElementById("chatbox");
 	var typing = document.getElementById("typing")
+	var joined = document.getElementById("joined")
+	var User; //not sure on how to get user and crn as of yet
+	var CRN;
 	var count=setInterval(function(){typing.innerHTML=""},10000)
+	var join_count=setInterval(function(){joined.innerHTML=""},10000)
 
 	//join chatroom
-	socket.emit('join',crn);
+	socket.emit('join',{crn: CRN, user: User});
+	clearInterval(join_count)
+	join_count=setInterval(function(){joined.innerHTML=""},10000)
+
+
+	$.ajax({
+		type: "GET",
+		datatype: JSON,
+		url: "../messages",
+		crn: CRN,
+		success: function(data){
+			for(var i=0; i < data.length;i++){
+				chatbox.innerHTML += "<p><strong>"+data[i].user+":<strong>"+data[i].message+"</p>"
+			}
+		}
+		error: function(){
+			alert("There is a problem")
+		}
+	})
 
 	//notify whose typing
 	text.addEventListener('keypress', function(){
 		clearInterval(count);
-		count=setInterval(timer,10000);
-		socket.emit("type", function(){
-			crn:"1",
-			user:"jason"
+		count=setInterval(function(){typing.innerHTML=""},10000)
+		socket.emit("type", {
+			crn: CRN,
+			user: User
 		})
 	})
 
@@ -23,13 +45,17 @@ function chat(){
 	btn.addEventListener('click', function(){
 		var current = new Date()
 		var now = current.getFullYear()+'-'+(current.getMonth()+1)+(current.getDate())+" "+current.getHours()+':'+current.getMinutes()+':'+current.getSeconds();
-		socket.emit('message',function(){
-		crn: "1",
-		user: "jason",
+		socket.emit('message',{
+		crn: CRN,
+		user: User,
 		message: text,
 		time: now,
 		})
 	});
+
+	socket.on("join",function(data){
+		joined.innerHTML=data.user + "has joined the room"
+	})
 
 	//display whose typing
 	socket.on("type", function(data){
@@ -43,7 +69,7 @@ function chat(){
 
 	//display saved messages
 	socket.on("display",function(data){
-		for(var i=0,i<data.length;i++){
+		for(var i=0;i<data.length;i++){
 			chatbox.innerHTML += "<p><strong>"+data[i].user+":<strong>"+data[i].message+"</p>"
 		}
 	})
--- socket/socket.js
+++ socket/socket.js
@@ -1,67 +1,64 @@
 const express = require('express');
+const app = express()
 const socket = require('socket.io');
 const db = require('../config/database');
-const message = require('../models/Message');
 
 //socket.io
 var users={};
 var rooms={}
-const io = socket(server);
-io.on('connection',(socket) =>{
-	console.log("connection")
+module.exports = function(io){
+	io.on('connection',(socket) =>{
+		console.log("connection")
 
-	//join user to a room
-	socket.on('join',(room) =>{
-		if(rooms[room] != []){
-			var temp = rooms[room]
-			temp.push(socket.id)
-		}
-		else{
-			rooms[room]=[socket.id]
-		}
-		socket.join(room)
-		//send the saved messages
-		message.findAll({order:[['time','DESC']],where:{rownum <= 30,crn=room}})
-		.then(messages =>{
-			io.to(socket.id).emit("display",messages)
-		})
-		.catch(err => console.log(err))s
-
-	//send messages to whoever is in the room
-	socket.on('message',(data) =>{
-		db.each('insert into messages values(?,?,?,?)',[data.crn,data.user,data.message,data.time])
-		io.to(data.crn).emit('chat',data)
-	})
-
-	//Check whose on
-	socket.on('on',(data) =>{
-		users[data.User]=socket.id;
-		console.log("There is "+Object.keys(users).length+" on")
-		io.sockets.emit('on',users);
-	});
-
-	//notify when someone is typing
-	socket.on('type',(data) => {
-		socket.broadcast.to(data.crn).emit('type',data);
-	})
-
-	socket.on('disconnect',function(){
-		for(var name in users){
-			if(users[name]==socket.id){
-				for(var room in rooms){
-					if(rooms[room].include(socket.id)){
-						var temp = rooms[room]
-						delete temp[socket.id]
-						socket.leave(room)
-						rooms[room]=temp
-					}
-				}
-				delete users[name]
-				io.sockets.emit('on',users)
+		//join user to a room
+		socket.on('join',(room) =>{
+			if(rooms[room] != []){
+				var temp = rooms[room]
+				temp.push(socket.id)
 			}
-		}
-		console.log("There is "+Object.keys(users).length+" on")
-		console.log('disconnected')
-	})
-	
-});
+			else{
+				rooms[room]=[socket.id]
+			}
+			socket.join(room)
+			io.to(data.crn).emit('join',room)
+		})
+
+		//send messages to whoever is in the room
+		socket.on('message',(data) =>{
+			db.each('insert into messages values(?,?,?,?)',[data.crn,data.user,data.message,data.time])
+			io.to(data.crn).emit('chat',data)
+		})
+
+		//Check whose on
+		socket.on('on',(data) =>{
+			users[data.User]=socket.id;
+			console.log("There is "+Object.keys(users).length+" on")
+			io.sockets.emit('on',users);
+		});
+
+		//notify when someone is typing
+		socket.on('type',(data) => {
+			socket.broadcast.to(data.crn).emit('type',data);
+		})
+
+		socket.on('disconnect',function(){
+			for(var name in users){
+				if(users[name]==socket.id){
+					for(var room in rooms){
+						if(rooms[room].include(socket.id)){
+							var temp = rooms[room]
+							delete temp[socket.id]
+							socket.leave(room)
+							rooms[room]=temp
+						}
+					}
+					delete users[name]
+					io.sockets.emit('on',users)
+				}
+			}
+			console.log("There is "+Object.keys(users).length+" on")
+			console.log('disconnected')
+		})
+		
+	});
+}
